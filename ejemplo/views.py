17c1e079efd359ccb0c31997e4bf7901123c608a
Osvaldo Castrejon <osvaldo.castrejon@sysviewsoft.com>
2022-10-11 21:29:17 -0500

    templates

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. 
+#Rivisar hora 8:50
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    sum = 15 + 18
+    return render(request, "ejemplo/saludar.html", 
+    {
+        "nombre":"Osvaldo",
+        "apellido":"Castrejon",
+        "suma": sum
+    })
+
+
+def index_dos (request, nombre, apellido):
+    return render(request, "ejemplo/saludar.html", 
+    {
+        "nombre":"Osvaldo",
+        "apellido":"Castrejon",
+    })
+
+def index_tres(request):
+    return render(request, "ejemplo/saludar.html",
+    {
+        "notas":[1,2,3,4,5,6,7,8]
+    })
+
+def imc(request, peso, altura):
+    imc = peso * altura
+    return render(request, "ejemplo/imc.html", {"imc": imc})
--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ejemplo.views import index
+from ejemplo.views import index, index_dos, index_tres, imc
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludar/', index),
+    path('saludar/<nombre>/<apellido>/', index_dos),
+    path('mostrar-notas/', index_tres),
+    path('calcular-imc/<peso>/<altura>/', imc),
 ]
